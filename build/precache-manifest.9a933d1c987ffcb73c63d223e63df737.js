fd43714ce1796ca9bea5c555c51aca14f38500af
Praveen Chandran <pc-mac@Praveens-MacBook-Air.local>
2020-03-30 21:33:32 +0400

    adding all the files and folders. this is for deploying this application to docker container

---
+++ build/precache-manifest.9a933d1c987ffcb73c63d223e63df737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccaa47fe6654cfe21b52232b0b06264",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "c5d10a61f77ca674fc82",
+    "url": "/monster/static/css/main.169b9e00.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5d10a61f77ca674fc82",
+    "url": "/monster/static/js/main.f6f1df53.chunk.js"
+  },
+  {
+    "revision": "09b6fb0b7ef1cdca7f8f",
+    "url": "/monster/static/js/runtime-main.f3aad347.js"
+  }
+]);
